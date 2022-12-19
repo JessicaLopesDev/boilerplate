e95923d7dbbc72a71c8f56ea52a279706f31908f
Jessica Lopes <jessicalopes.dev@gmail.com>
2022-12-19 18:46:35 -0300

    pwa funcionando corretamente

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
-};
+const isProd = process.env.NODE_ENV === 'production';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const runtimeCaching = require('next-pwa/cache');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd,
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/]
+});
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['fr'],
+    defaultLocale: 'fr'
+  }
+});
 module.exports = nextConfig;
